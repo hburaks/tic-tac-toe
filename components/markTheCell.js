2558b72882dd2463fa43006c7b5203cd24d505b8
hburaks <hasanburaksongur@gmail.com>
2023-04-24 20:51:53 +0300

    refactor to keep playing in spots already taken

--- components/markTheCell.js
+++ components/markTheCell.js
@@ -3,11 +3,12 @@ import toggleActivePlayer from "./toggleActivePlayer.js";
 export default function markTheCell() {
 	cells.forEach((cell) => {
 		cell.addEventListener("click", () => {
-			cell.style.color = "black";
-			cell.dataset.cell = activePlayer.activePlayer.mark;
-			toggleActivePlayer(activePlayer);
-			console.log(activePlayer.activePlayer);
-			cell.innerText = cell.dataset.cell;
+			if (cell.dataset.cell === "") {
+				cell.style.color = "black";
+				cell.dataset.cell = activePlayer.activePlayer.mark;
+				toggleActivePlayer(activePlayer);
+				cell.innerText = cell.dataset.cell;
+			}
 		});
 	});
 }
